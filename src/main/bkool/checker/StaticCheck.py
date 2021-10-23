1faecc4137c98a113e3e8ac5fb376ac31497ba2e
dopham123 <34420891+dopham123@users.noreply.github.com>
2021-10-23 23:16:58 +0700

    next change

--- src/main/bkool/checker/StaticCheck.py
+++ src/main/bkool/checker/StaticCheck.py
@@ -8,23 +8,13 @@ from Utils import Utils
 from StaticError import *
 import copy
 
+from main.bkool.utils.AST import BoolType, FloatType, IntType, MethodDecl, StringType, VarDecl
+
 class Type(ABC):
     __metaclass__ = ABCMeta
     pass
 class Prim(Type):
     __metaclass__ = ABCMeta
-    pass
-class IntType(Prim):
-    pass
-class FloatType(Prim):
-    pass
-class StringType(Prim):
-    pass
-class BoolType(Prim):
-    pass
-class VoidType(Type):
-    pass
-class Unknown(Type):
     pass
 class ClassTyp(Type):
     def __init__(self, classname, parrentname = None):
@@ -73,7 +63,8 @@ class StaticChecker(BaseVisitor,Utils):
     def __init__(self,ast):
         self.ast = ast
 
- 
+    def visit(self,ast,param):
+        return ast.accept(self,param)
     
     def check(self):
         return self.visit(self.ast,StaticChecker.global_envi)
@@ -92,7 +83,7 @@ class StaticChecker(BaseVisitor,Utils):
         
         #lưu tên memdecl vào <symbol>.value nếu là super class
         for x in ast.decl:
-            if not x.parentname.name:
+            if not x.parentname:
                 for z in c:
                     if z.name==x.classname.name: #tìm class trong env ứng với x
                         for y in x.memlist: #y: MethodDecl or AttributeDecl
@@ -112,7 +103,7 @@ class StaticChecker(BaseVisitor,Utils):
                                             raise Redeclared(Method(), y.name)
                                         else:
                                             #kiểm tra kiểu của khai báo const
-                                            if isinstance(y.decl.constType, self.visit(y.decl.value)): #check type constdecl
+                                            if y.decl.constType == self.visit(y.decl.value, c): #check type constdecl
                                                 z.value.get("global_sc_child").append(Symbol(y.decl.constant.name, y.decl.constType, y.decl.constType))
                                             else:
                                                 raise TypeMismatchInConstant(y)
@@ -122,6 +113,8 @@ class StaticChecker(BaseVisitor,Utils):
                                         if y.decl.variable.name in [child.name for child in z.value.get("global_sc_child")]:
                                             raise Redeclared(Method(), y.name)
                                         else:
+                                            if y.decl.varInit:
+                                                self.visit(y.decl.varInit, c)
                                             z.value.get("global_sc_child").append(Symbol(y.decl.variable.name, y.decl.varType))
                                 #Instance
                                 else:
@@ -131,7 +124,11 @@ class StaticChecker(BaseVisitor,Utils):
                                             raise Redeclared(Method(), y.name)
                                         else:
                                             #kiểm tra kiểu của khai báo const
-                                            if isinstance(y.decl.constType, self.visit(y.decl.value)): #check type constdecl
+                                            print(type(y.decl.constType))
+                                            print('==================')
+                                            print(isinstance(type(y.decl.constType), type(self.visit(y.decl.value, c))))
+                                            print(type(self.visit(y.decl.value, c)))
+                                            if y.decl.constType == self.visit(y.decl.value, c): #check type constdecl
                                                 z.value.get("class_sc_child").append(Symbol(y.decl.constant.name, y.decl.constType, y.decl.constType))
                                             else:
                                                 raise TypeMismatchInConstant(y)
@@ -139,13 +136,16 @@ class StaticChecker(BaseVisitor,Utils):
                                     else:
                                         #kiểm tra khai báo trùng id
                                         if y.decl.variable.name in [child.name for child in z.value.get("class_sc_child")]:
-                                            raise Redeclared(Method(), y.name)
+                                            raise Redeclared(Method(), y.decl.variable.name)
                                         else:
+                                            if y.decl.varInit:
+                                                self.visit(y.decl.varInit, c)
                                             z.value.get("class_sc_child").append(Symbol(y.decl.variable.name, y.decl.varType))
                         break
 
+        #cập nhật môi trường cho subclass
         for x in ast.decl:
-            if x.parentname.name:
+            if x.parentname:
                 for z in c:
                     if z.name==x.classname.name: #tìm class trong env ứng với x
                         for k in c: # tìm class trong env ứng với class cha của x
@@ -168,7 +168,7 @@ class StaticChecker(BaseVisitor,Utils):
                                             raise Redeclared(Method(), y.name)
                                         else:
                                             #kiểm tra kiểu của khai báo const
-                                            if isinstance(y.decl.constType, self.visit(y.decl.value)): #check type constdecl
+                                            if y.decl.constType == self.visit(y.decl.value, c): #check type constdecl
                                                 z.value.get("global_sc_child").append(Symbol(y.decl.constant.name, y.decl.constType, y.decl.constType))
                                             else:
                                                 raise TypeMismatchInConstant(y)
@@ -178,6 +178,8 @@ class StaticChecker(BaseVisitor,Utils):
                                         if y.decl.variable.name in [child.name for child in z.value.get("global_sc_child")]:
                                             raise Redeclared(Method(), y.name)
                                         else:
+                                            if y.decl.varInit:
+                                                self.visit(y.decl.varInit, c)
                                             z.value.get("global_sc_child").append(Symbol(y.decl.variable.name, y.decl.varType))
                                 #Instance
                                 else:
@@ -187,7 +189,7 @@ class StaticChecker(BaseVisitor,Utils):
                                             raise Redeclared(Method(), y.name)
                                         else:
                                             #kiểm tra kiểu của khai báo const
-                                            if isinstance(y.decl.constType, self.visit(y.decl.value)): #check type constdecl
+                                            if y.decl.constType == self.visit(y.decl.value, c): #check type constdecl
                                                 z.value.get("class_sc_child").append(Symbol(y.decl.constant.name, y.decl.constType, y.decl.constType))
                                             else:
                                                 raise TypeMismatchInConstant(y)
@@ -197,6 +199,8 @@ class StaticChecker(BaseVisitor,Utils):
                                         if y.decl.variable.name in [child.name for child in z.value.get("class_sc_child")]:
                                             raise Redeclared(Method(), y.name)
                                         else:
+                                            if y.decl.varInit:
+                                                self.visit(y.decl.varInit, c)
                                             z.value.get("class_sc_child").append(Symbol(y.decl.variable.name, y.decl.varType))
                         break
 
@@ -205,7 +209,107 @@ class StaticChecker(BaseVisitor,Utils):
     def returnParamType(param): #param: list[VarDecl]
         [x.varType for x in param]
 
+    def visitClassDecl(self, ast, c):
+        # classname : Id
+        # memlist : List[MemDecl]
+        # parentname : Id = None # None if there is no parent
+        for x in ast.memlist:
+            if type(x) is MethodDecl:
+                obj = [[]] + [c]
+                self.visit(x, obj)
+
+    def visitMethodDecl(self, ast, o):
+        # kind: SIKind
+        # name: Id
+        # param: List[VarDecl]
+        # returnType: Type  # None for constructor
+        # body: Block
+        for x in ast.param:
+            self.visit(x, o)
+
+        self.visit(ast.body, o)
+
+    def visitVarDecl(self, ast, o):
+        # variable : Id
+        # varType : Type
+        # varInit : Expr = None # None if there is no initial
+        for x in o[0]:
+            if ast.variable.name == x.name:
+                raise Redeclared(Variable(), ast.variable.name)
+
+        if ast.varInit:
+            self.visit(ast.varInit, o)
+        
+        # if ast.varType != self.visit(ast.varInit, o):
+        #     raise 
+
+        o[0].append(Symbol(ast.variable.name, ast.varType))
+
+    def visitConstDecl(self, ast, o):
+        # constant : Id
+        # constType : Type
+        # value : Expr
+        for x in o[0]:
+            if ast.constant.name == x.name:
+                raise Redeclared(Constant(), ast.constant.name)
+
+        if ast.constType != self.visit(ast.value, o):
+            raise TypeMismatchInConstant(ast)
+
+        o[0].append(Symbol(ast.constant.name, ast.constType, ast.constType))
     
+    def visitBlock(self, ast, o):
+        # decl:List[StoreDecl]
+        # stmt:List[Stmt]
+        [self.visit(x, o) for x in ast.decl]
+
+        [self.visit(x, o) for x in ast.stmt]
+    
+    def visitBinaryOp(self, ast, o):
+        # op:str
+        # left:Expr
+        # right:Expr
+        type1 = self.visit(ast.left, o)
+        type2 = self.visit(ast.right, o)
+        if ast.op in ['\\', '%']:
+            if type(type1) is not IntType or type(type2) is not IntType:
+                raise TypeMismatchInExpression(ast)
+            return IntType()
+
+        elif ast.op in ['+', '-', '*', '/']:
+            if type(type1) not in [IntType, FloatType] or type(type2) not in [IntType, FloatType]:
+                raise TypeMismatchInExpression(ast)
+            elif type1!=type2 or ast.op == '/':
+                return FloatType()
+            else:
+                return type1
+
+        elif ast.op in ['&&', '||', '!']:
+            if type(type1) is BoolType and type(type2) is BoolType:
+                return BoolType()
+            else:
+                raise TypeMismatchInExpression(ast)
+
+        elif ast.op in ['==', '!=']:
+            if type(type1) not in [IntType, BoolType] or type(type2) not in [IntType, BoolType] or type1 != type2:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return BoolType()
+
+        elif ast.op in ['>', '<', '>=', '<=']:
+            if type(type1) not in [IntType, FloatType] or type(type2) not in [IntType, FloatType]:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return BoolType()
+
+        elif ast.op in ['^']:
+            if type(type1) is not StringType or type(type2) is not StringType:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return StringType()
+        
+        
+
     def visitFuncDecl(self,ast, c): 
         return list(map(lambda x: self.visit(x,(c,True)),ast.body.stmt)) 
     
--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -5,50 +5,50 @@ from AST import *
 class CheckerSuite(unittest.TestCase):
     def test_undeclared_function(self):
         """Simple program: int main() {} """
-        input = """int main() {foo();}"""
+        input = Program([ClassDecl(Id("main"),[AttributeDecl(Instance(),VarDecl(Id("a"),IntType(),IntLiteral(5))),AttributeDecl(Instance(),ConstDecl(Id("v"),IntType(),IntLiteral(6))),AttributeDecl(Instance(),VarDecl(Id("a"),IntType(),IntLiteral(5)))])])
         expect = "Undeclared Function: foo"
         self.assertTrue(TestChecker.test(input,expect,400))
 
-    def test_diff_numofparam_stmt(self):
-        """More complex program"""
-        input = """class main {
-            int a = 5;
-            final int v = 6;
-            int a
-        }"""
-        expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
-        self.assertTrue(TestChecker.test(input,expect,401))
+    # def test_diff_numofparam_stmt(self):
+    #     """More complex program"""
+    #     input = """class main {
+    #         int a = 5;
+    #         final int v = 6;
+    #         int a
+    #     }"""
+    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    #     self.assertTrue(TestChecker.test(input,expect,401))
     
-    def test_diff_numofparam_expr(self):
-        """More complex program"""
-        input = """int main () {
-            putIntLn(getInt(4));
-        }"""
-        expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
-        self.assertTrue(TestChecker.test(input,expect,402))
+    # def test_diff_numofparam_expr(self):
+    #     """More complex program"""
+    #     input = """int main () {
+    #         putIntLn(getInt(4));
+    #     }"""
+    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+    #     self.assertTrue(TestChecker.test(input,expect,402))
 
-    def test_undeclared_function_use_ast(self):
-        """Simple program: int main() {} """
-        input = Program([FuncDecl(Id("main"),[],IntType(),Block([],[
-            CallExpr(Id("foo"),[])]))])
-        expect = "Undeclared Function: foo"
-        self.assertTrue(TestChecker.test(input,expect,403))
+    # def test_undeclared_function_use_ast(self):
+    #     """Simple program: int main() {} """
+    #     input = Program([FuncDecl(Id("main"),[],IntType(),Block([],[
+    #         CallExpr(Id("foo"),[])]))])
+    #     expect = "Undeclared Function: foo"
+    #     self.assertTrue(TestChecker.test(input,expect,403))
 
-    def test_diff_numofparam_expr_use_ast(self):
-        """More complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],IntType(),Block([],[
-                    CallExpr(Id("putIntLn"),[
-                        CallExpr(Id("getInt"),[IntLiteral(4)])
-                        ])]))])
-        expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
-        self.assertTrue(TestChecker.test(input,expect,404))
+    # def test_diff_numofparam_expr_use_ast(self):
+    #     """More complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],IntType(),Block([],[
+    #                 CallExpr(Id("putIntLn"),[
+    #                     CallExpr(Id("getInt"),[IntLiteral(4)])
+    #                     ])]))])
+    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+    #     self.assertTrue(TestChecker.test(input,expect,404))
 
-    def test_diff_numofparam_stmt_use_ast(self):
-        """More complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],IntType(),Block([],[
-                    CallExpr(Id("putIntLn"),[])]))])
-        expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
-        self.assertTrue(TestChecker.test(input,expect,405))
+    # def test_diff_numofparam_stmt_use_ast(self):
+    #     """More complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],IntType(),Block([],[
+    #                 CallExpr(Id("putIntLn"),[])]))])
+    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    #     self.assertTrue(TestChecker.test(input,expect,405))
     
